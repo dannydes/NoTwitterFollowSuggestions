5ad45cf793a44fef9e1177f517a551e755d60c47
Daniel <desiradaniel2007@hotmail.com>
2014-06-11 19:02:20 +0200

    Initial commit

---
+++ data/suggestions_rem.js
@@ -0,0 +1,10 @@
+(function () {
+	'use strict';alert('124554');
+
+	let wtfNt = document.getElementsByClassName('ProfileWTFAndTrends');
+
+	if (wtfNt.length !== 0) {
+		let wtf = document.getElementsByClassName('WhoToFollow')[0];
+		wtfNt[0].removeChild(wtf);
+	}
+})();
