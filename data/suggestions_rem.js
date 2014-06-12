535a5ed424645b18a63160fe734be4ddfee72342
Daniel <desiradaniel2007@hotmail.com>
2014-06-12 18:08:13 +0200

    Follow suggestions are removed for home, notifications & discover

--- data/suggestions_rem.js
+++ data/suggestions_rem.js
@@ -1,10 +1,22 @@
 (function () {
-	'use strict';alert('124554');
+	'use strict';
 
-	let wtfNt = document.getElementsByClassName('ProfileWTFAndTrends');
+	function removeSuggestions() {
+		let wtfNt = document.getElementsByClassName('ProfileWTFAndTrends');
 
-	if (wtfNt.length !== 0) {
-		let wtf = document.getElementsByClassName('WhoToFollow')[0];
-		wtfNt[0].removeChild(wtf);
+		if (wtfNt.length) {
+			let wtf = wtfNt[0].getElementsByClassName('WhoToFollow')[0];
+			wtfNt[0].removeChild(wtf);
+		}
+
+		let rDashboard = document.getElementsByClassName('dashboard-right');
+
+		if (rDashboard.length) {
+			let wtf = rDashboard[0].getElementsByClassName('wtf-module')[0];
+			rDashboard[0].removeChild(wtf);
+		}
 	}
+
+	addEventListener('paint', removeSuggestions);
+	removeSuggestions();
 })();
